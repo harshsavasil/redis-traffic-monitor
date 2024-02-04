5da7a5b97acaf64f4022e328d04fa3d60a32311c
Parv Jain <parv.jain@pluang.com>
2024-02-04 17:35:38 +0530

    refactored

---
+++ config/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    redisConfig: {
+        port: 6379,
+    },
+    influxDBConfig: {
+        url: 'http://localhost:8086',
+        token: 'YpUl8QwkrtPSttB9Eu26j9d2xXhZ1JifZmrcu3okWuwyR0XRN7NCBMDeK9uKMNylqrZW11myxbUK9gO_ERF2eg==',
+        org: 'localorg',
+        bucket: 'localbucket',
+    },
+    networkInterface: 'lo0',
+};
