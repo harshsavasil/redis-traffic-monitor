6ffe22e9ee56bafbee30e370a2e6b04c73a78bb2
Parv Jain <parv.jain@pluang.com>
2024-02-07 11:19:07 +0530

    docker file added

--- config/index.js
+++ config/index.js
@@ -1,12 +1,12 @@
 module.exports = {
     redisConfig: {
-        port: 6379,
+        port: process.env.REDIS_PORT || 6379,
     },
     influxDBConfig: {
-        url: 'http://localhost:8086',
-        token: 'YpUl8QwkrtPSttB9Eu26j9d2xXhZ1JifZmrcu3okWuwyR0XRN7NCBMDeK9uKMNylqrZW11myxbUK9gO_ERF2eg==',
-        org: 'localorg',
-        bucket: 'localbucket',
+        url: process.env.INFLUX_DB_URL || 'http://localhost:8086',
+        token: process.env.INFLUX_DB_TOKEN || 'YpUl8QwkrtPSttB9Eu26j9d2xXhZ1JifZmrcu3okWuwyR0XRN7NCBMDeK9uKMNylqrZW11myxbUK9gO_ERF2eg==',
+        org: process.env.INFLUX_DB_ORG || 'localorg',
+        bucket: process.env.INFLUX_DB_BUCKET || 'localbucket',
     },
-    networkInterface: 'lo0',
+    networkInterface: process.env.NETWORK_INTERFACE || 'lo0',
 };
