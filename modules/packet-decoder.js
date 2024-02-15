e4f76e3d148fa68e9c0220da671e7ac100b7c47e
Parv Jain <parv.jain@pluang.com>
2024-02-15 20:32:44 +0530

    process packet algo updated

--- modules/packet-decoder.js
+++ modules/packet-decoder.js
@@ -7,8 +7,9 @@ class PacketDecoder {
         this.queries = {};
         this.logger = logger;
         this.queryProcessor = new QueryProcessor({ logger: this.logger });
-        this.pcapSession = pcap.createSession(config.networkInterface);
+        this.pcapSession = pcap.createSession(config.networkInterface, {filter: 'tcp and port 6379'});
         this.fragmentsMapByIdentification = new Map();
+        this.segmentMapByNextSeqno = new Map();
     }
 
     start() {
@@ -18,7 +19,7 @@ class PacketDecoder {
         this.logger.info('Started packet decoder');
     }
 
-    processPacket(rawPacket) {
+    processPacketReassembleAlgo(rawPacket) {
         try {
             const packet = pcap.decode.packet(rawPacket);
             const ipv4Packet = packet.payload.payload;
@@ -29,7 +30,7 @@ class PacketDecoder {
             const isRedisRequest = dport === config.redisConfig.port;
             const isRedisResponse = sport === config.redisConfig.port;
             if (data && (isRedisRequest || isRedisResponse)) {
-                let isDatagramFragmented = moreFragments || fragmentOffset > 0;
+                let isDatagramFragmented = !(moreFragments === false && fragmentOffset === 0);
                 // if (doNotFragment) {
                 //     this.logger.error({ headerLength, length, flags, identification, fragmentOffset }, 'Packet has doNotFragment set - it can\'t be fragmented');
                 //     return;
@@ -50,20 +51,63 @@ class PacketDecoder {
                 this.fragmentsMapByIdentification.set(identification, fragments);
                 if (moreFragments === false) {
                     // this is last fragment
-                    let mergedData = Buffer.from([]);
+                    let mergedData = Buffer.alloc(0);
                     let expectedNextFragmentOffset = 0;
                     const fragmentOffsets = Object.keys(fragments).sort((a, b) => a - b);
                     fragmentOffsets.forEach((fragmentOffset) => {
-                        if (fragmentOffset === expectedNextFragmentOffset) {
+                        if (fragmentOffset == expectedNextFragmentOffset) {
                             const fragment = fragments[fragmentOffset];
                             expectedNextFragmentOffset = fragment.nextFragmentOffset;
-                            mergedData = Buffer.concat(mergedData, fragment.value);
+                            mergedData = Buffer.concat([mergedData, fragment.value]);
                         } else {
                             this.logger.error({ fragmentOffsets, fragmentOffset, expectedNextFragmentOffset }, 'Not able to find expected fragment offset');
                         }
                     });
                     this.emitPacket(isRedisRequest, isRedisResponse, ackno, seqno, mergedData); 
                     this.fragmentsMapByIdentification.delete(identification);
+                }
+            }
+        } catch (err) {
+            this.logger.error(err, 'Error processing network packet');
+        }
+    }
+
+    processPacket(rawPacket) {
+        try {
+            const packet = pcap.decode.packet(rawPacket);
+            const ipv4Packet = packet.payload.payload;
+            if (ipv4Packet.protocol !== 6) return;
+            const { payload } = ipv4Packet;
+            const { ackno, seqno, dport, sport, data, dataLength } = payload;
+            const { psh } = payload.flags;
+            const isRedisRequest = dport === config.redisConfig.port;
+            const isRedisResponse = sport === config.redisConfig.port;
+            if (data && (isRedisRequest || isRedisResponse)) {
+                const expectedNextSeqno = seqno + dataLength;
+                this.segmentMapByNextSeqno.set(expectedNextSeqno, {
+                    seqno,
+                    ackno,
+                    data: Buffer.from(data), // Create a new Buffer object,
+                    dataLength,
+                });
+                if (psh) {
+                    // last segment
+                    const chunks = [];
+                    let current = expectedNextSeqno;
+                    while(current) {
+                        const segment = this.segmentMapByNextSeqno.get(current);
+                        if (segment) {
+                            chunks.unshift(segment);
+                            this.segmentMapByNextSeqno.delete(expectedNextSeqno);
+                            current = segment.seqno;
+                        } else {
+                            current = null;
+                        }
+                    }
+                    const mergedData = Buffer.concat(chunks.map((chunk) => chunk.data));
+                    const mergedSegment = chunks[0];
+                    mergedSegment.data = mergedData;
+                    this.emitPacket(isRedisRequest, isRedisResponse, mergedSegment.ackno, mergedSegment.seqno, mergedSegment.data);
                 }
             }
         } catch (err) {
